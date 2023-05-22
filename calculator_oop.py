bfd75b7ac63228ea9a3b966d43c42d1a342d35c2
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-22 19:24:11 +0800

    first commit

---
+++ calculator_oop.py
@@ -0,0 +1,21 @@
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        
+
+    def add(self, num1, num2):
+        return self.num1 + self.num2
+        
+
+# take user input for numbers and operation
+num1 = float(input("Enter first number:"))
+operation = input("Enter operator [+, -, *, /]:")
+num2 = float(input("Enter second number:"))
+
+calc = Calculator(self, num1, num2)
+
+#print the sum of two numbers
+if operation == "+"
+print(num1, "+", num2, "=", calc.add())
+    
