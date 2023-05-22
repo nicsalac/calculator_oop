8b8ad3f2a659529a869d8742bf42446f329f7ad0
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-22 22:05:14 +0800

    Seperate Class for User Input

---
+++ calculator_for_user_input.py
@@ -0,0 +1,12 @@
+class UserInput:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+# take user input for numbers and operation
+num1 = float(input("Enter first number:"))
+operation = input("Enter operator [+, -, *, /]:")
+num2 = float(input("Enter second number:"))
+
+
+    
