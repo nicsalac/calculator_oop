86e66dd15bae2734c8f5168e57ddc6c434bfea8a
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-22 19:57:47 +0800

    addition operator

--- calculator_oop.py
+++ calculator_oop.py
@@ -4,7 +4,7 @@ class Calculator:
         self.num2 = num2
         
 
-    def add(self, num1, num2):
+    def add(self):
         return self.num1 + self.num2
         
 
@@ -13,9 +13,9 @@ num1 = float(input("Enter first number:"))
 operation = input("Enter operator [+, -, *, /]:")
 num2 = float(input("Enter second number:"))
 
-calc = Calculator(self, num1, num2)
+calc = Calculator(num1, num2)
 
 #print the sum of two numbers
-if operation == "+"
-print(num1, "+", num2, "=", calc.add())
+if operation == '+':
+    print(num1, "+", num2, "=", calc.add())
     
