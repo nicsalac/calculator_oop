1d215e6a05bec26021059184661005d6a5409de0
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-27 03:27:53 +0800

    final commits for the program

--- program_oop_calculator.py
+++ program_oop_calculator.py
@@ -1,14 +1,18 @@
 from calculator_oop import Calculator
 from calculator_for_user_input import UserInput
+
+ui = UserInput()
+num1, num2, operation = ui.get_user_input()
 calc = Calculator(num1, num2)
-ui = UserInput
 
 #perform any operations based on user input
 if operation == '+':
-    print(ui.num1, "+", ui.num2, "=", calc.add())
-if operation == '-':
-    print(ui.num1, "-", ui.num2, "=", calc.subtract()) 
-if operation == '*':
-    print(ui.num1, '*', ui.num2, '=', calc.multiply())
-if operation == '/':
-    print(ui.num1, '/', ui.num2, '=', calc.divide())
+    print(num1, "+", num2, "=", calc.add())
+elif operation == '-':
+    print(num1, "-", num2, "=", calc.subtract()) 
+elif operation == '*':
+    print(num1, '*', num2, '=', calc.multiply())
+elif operation == '/':
+    print(num1, '/', num2, '=', calc.divide())
+else:
+    print("Invalid operator entered.")
