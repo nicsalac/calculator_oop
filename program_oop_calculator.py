9f3b44164613e4a6344db62f1dff99ea7511268c
nicsalac <nicoletteirishsalac127@gmail.com>
2023-06-03 18:52:25 +0800

    import the inherits class and add the operation the could get the square of an integer

--- program_oop_calculator.py
+++ program_oop_calculator.py
@@ -1,9 +1,13 @@
 from calculator_oop import Calculator
+from calculator_oop import NewCalculator
 from calculator_for_user_input import UserInput
+
+
 
 ui = UserInput()
 num1, num2, operation = ui.get_user_input()
 calc = Calculator(num1, num2)
+newcalc = NewCalculator(num1, num2)
 
 #perform any operations based on user input
 if operation == '+':
@@ -14,5 +18,7 @@ elif operation == '*':
     print(num1, '*', num2, '=', calc.multiply())
 elif operation == '/':
     print(num1, '/', num2, '=', calc.divide())
+elif operation == 'square' :
+    print("square of" ,num1, "is", newcalc.square, "and the square of", num2, "is", newcalc.square())
 else:
     print("Invalid operator entered.")
